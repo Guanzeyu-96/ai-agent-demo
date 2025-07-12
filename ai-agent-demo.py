7ad009b515a5d628119a0b6b8dd4c002a7a497dc
Zeyu Guan <zeyuguan@ZeyudeMac-mini.local>
2025-07-12 18:19:39 +0800

    Initial commit: AI agent project

---
+++ ai-agent-demo.py
@@ -0,0 +1,31 @@
+#### main.py
+from pydantic_ai import Agent
+from pydantic_ai.models.openai import OpenAIModel
+from pydantic_ai.providers.deepseek import DeepSeekProvider
+
+from dotenv import load_dotenv
+import tools
+import os
+
+load_dotenv()
+model = OpenAIModel(
+    'deepseek-chat',
+    provider=DeepSeekProvider(api_key=os.getenv('DEEPSEEK_API_KEY')),
+)
+
+agent = Agent(model,
+              system_prompt="You are an experienced programmer",
+              tools=[tools.read_file, tools.list_files, tools.rename_file])
+
+def main():
+    history = []
+    while True:
+        user_input = input("Input: ")
+        resp = agent.run_sync(user_input,
+                              message_history=history)
+        history = list(resp.all_messages())
+        print(resp.output)
+
+
+if __name__ == "__main__":
+    main()
